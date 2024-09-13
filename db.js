a07e33eab403edfaf68370327caa4c601d266083
DimpleKA <153168007+DimpleKA@users.noreply.github.com>
2024-09-14 01:42:47 +0530

    ssdg

--- db.js
+++ db.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB=async()=>{
     // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/criminalDB', {
+// const uri ="mongodb://localhost:27017/criminalDB";
+const uri ="mongodb+srv://Dimpleusername:Dimple9@cluster0.l6frpvy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/criminalDB";
+
+
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
